163ebd67c380b9558b90a302e6696d59885c7a50
NeoBoledi <75299402+NeoBoledi@users.noreply.github.com>
2022-03-12 04:34:15 +0200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,14 +7,11 @@
   */
 int main(void)
 {
-	char n;
-
-	for (n = '0'; n <= '9'; n++)
-	{
-		putchar(n);
-	}
-
-	putchar('\n');
-
-	return (0);
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
