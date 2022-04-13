a860e7294567ad38fe73105d4f4ecfe8bc42abae
NeoBoledi <75299402+NeoBoledi@users.noreply.github.com>
2022-04-14 01:41:39 +0200

    To hell with circumstances

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,39 +1,29 @@
 #include "function_pointers.h"
+
 /**
-  *int_index - searches for an integer.
-  *@array: array of integers.
-  *@size: number of elements in array.
-  *@cmp: pointer to function used to compare values.
-  *
-  *Return: index of first element.
-  *-1 if no element is found or siz <= 0.
-  */
+ * int_index - searches for an integer
+ *
+ * @array: an array of integers
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare
+ *
+ * Return: index of the first element, -1 if size <= 0
+ * or if no element matches
+ *
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, res;
+	int i;
 
-	res = -1;
-	if (array && cmp)
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-
-		if (size <= 0)
-		{
-			return (res);
-		}
-		for (i = 0; i < size; i++)
-		{
-			cmp(array[i]);
-			if (cmp(array[i]) > 0)
-			{
-				res = i;
-				break;
-			}
-			if ((cmp(array[i]) == (-1)))
-			{
-				return (res);
-			}
-		}
-
+		if (cmp(array[i]))
+			return (i);
 	}
-	return (res);
+
+	return (-1);
 }
